79d91f75c336ce19734f0151cc22f9fe289ffc9c
Morenayd <tofdebby@gmail.com>
2024-07-24 11:49:55 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jesutofunmi Adewole on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
